fce0c0fbfc31df4d389ff7a895767528d2175523
Krish <krishvusf@gmail.com>
2024-01-25 16:16:50 -0500

    Robin is so smart

---
+++ IEEEpasswordGenerator.py
@@ -0,0 +1,12 @@
+import random
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@$%^&*()#'
+number = input('Amount of passwords you want: ')
+number = int(number)
+length = input('What do you want the length of your passwords to be: ')
+length = int(length)
+print('\nYour passwords are as follows: ')
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+    print(passwords)
