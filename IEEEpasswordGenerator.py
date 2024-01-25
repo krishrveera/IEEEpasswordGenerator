b64a7296c6068e3d40a22e10df46eeb975af8e28
Krish <krishvusf@gmail.com>
2024-01-25 16:22:22 -0500

    Workshop commit

--- IEEEpasswordGenerator.py
+++ IEEEpasswordGenerator.py
@@ -1,3 +1,4 @@
+# This code is to create a tool that would generate different passwords
 import random
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@$%^&*()#'
 number = input('Amount of passwords you want: ')
